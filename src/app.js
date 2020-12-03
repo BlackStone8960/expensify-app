2258156c56c79d270bbe358f98628c3dadd6dedf
BlackStone <fng.trf.kk1020@gmail.com>
2020-12-03 14:32:26 +0900

    Setup production webpack build

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense, removeExpense, editExpense } from './actions/expenses';
-import { setTextFilter, sortByAmount, sortByDate, setStartDate, setEndDate } from './actions/filters';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
@@ -12,7 +12,6 @@ import 'react-dates/lib/css/_datepicker.css'
 
 // creating store
 const store = configureStore();
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
